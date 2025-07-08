81404aa6ae934405967dd61aff01d2e5ed7200d1
Bernard Krehula <krehula.bernard456@gmail.com>
2025-07-08 18:28:37 +0200

    Open github link on learn more

--- my-app/src/Game.jsx
+++ my-app/src/Game.jsx
@@ -28,6 +28,9 @@ const SingleGame = ({game, displayAddNewGame, getGameValues, setIsEdited }) => {
       getGameValues(newGame);
       displayAddNewGame();
     }
+    const handleLearnMore = () => {
+      window.open('https://github.com/bernardkrehula');
+    }
 
     return(
         <>
@@ -55,7 +58,7 @@ const SingleGame = ({game, displayAddNewGame, getGameValues, setIsEdited }) => {
               <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-category"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4h6v6h-6z" /><path d="M14 4h6v6h-6z" /><path d="M4 14h6v6h-6z" /><path d="M17 17m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" /></svg>
               <span >{type}</span>
             </div>
-            <Btn variation='learn-more'>Learn More</Btn>
+            <Btn variation='learn-more' onClick={() => {handleLearnMore()}}>Learn More</Btn>
             <Btn variation='edit' type='submit' onClick={() => {handleClick()}}>Edit</Btn>
           </li>
         </>
